ff79cc10c1fb28aba54cd942434678103b556698
Joao Bazzo <joao.bazzo@gmail.com>
2022-08-16 17:14:12 -0300

    code to check and build

--- R/build.R
+++ R/build.R
@@ -1,2 +1,12 @@
 # An optional custom script to run before Hugo builds your site.
 # You can delete it if you do not need it.
+blogdown::check_site()
+
+
+blogdown::stop_server()
+blogdown::build_site()
+blogdown::new_content()
+blogdown::new_post(title = "gtfs2emis package")
+
+
+blogdown::serve_site()
